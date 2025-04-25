99c60c3e89b97137f139eb8ea7f31ef4ba6b050e
Afcisufhsoufh <mehedir316@gmail.com>
2025-04-25 18:57:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # API credentials (replace with your own)
-API_ID = "Your API ID"
-API_HASH = "Your API Hash"
-BOT_TOKEN = "Your Bot Token"
+API_ID = "28239710"
+API_HASH = "7fc5b35692454973318b86481ab5eca3"
+BOT_TOKEN = "8008001642:AAEN5DByAdRx8HtRqj_RYnIQGLP7CaqZn0o"
 COMMAND_PREFIX = [",", ".", "/", "!"]
